f615a9a46d717325d6dc61df901b045f3ffc978c
Johnson Bhaskaran <johnsonb@live.in>
2025-10-18 23:18:02 +0530

    New: Quote component - hero section of the portfolio

---
+++ src/sections/Quote.jsx
@@ -0,0 +1,9 @@
+const Quote = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <h3 className='text-7xl'>A pinch of curiosity & a bit of enthusiasm goes a long way</h3>
+      <p className='text-4xl'> -unknown</p>
+    </div>
+  );
+};
+export default Quote;
