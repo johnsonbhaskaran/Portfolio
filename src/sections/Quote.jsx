c0c275e6caee9d327d4adddd2887a0a2b6d539cc
Johnson Bhaskaran <johnsonb@live.in>
2025-10-27 18:03:19 +0530

    Change: made changes to the line height, font size and spacing

--- src/sections/Quote.jsx
+++ src/sections/Quote.jsx
@@ -1,8 +1,12 @@
 const Quote = () => {
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <h3 className='text-7xl'>A pinch of curiosity & a bit of enthusiasm goes a long way</h3>
-      <p className='text-4xl'> -unknown</p>
+    <div className='flex justify-center'>
+      <div className='flex flex-col justify-center items-center max-w-5xl h-screen'>
+        <h3 className='mb-6 text-7xl/23'>
+          A pinch of curiosity & a bit of enthusiasm goes a long way
+        </h3>
+        <p className='w-full text-4xl text-right'> -unknown</p>
+      </div>
     </div>
   );
 };
