cfa782c118687633d0185668e9e4151cbfec933e
Johnson Bhaskaran <johnsonb@live.in>
2025-10-27 18:03:51 +0530

    Done: theme switcher toggle completed

--- src/components/Sun.jsx
+++ src/components/Sun.jsx
@@ -5,22 +5,24 @@ import SunOFF from "../assets/Sun-OFF.svg";
 const Sun = () => {
   const [darkmode, setDarkmode] = useState(false);
   return (
-    <div className='top-0 left-0 z-20 sticky size-16 sm:size-full'>
-      {darkmode ? (
-        <img
-          onClick={() => setDarkmode(!darkmode)}
-          className='cursor-pointer'
-          src={SunON}
-          alt='theme swtich icon'
-        />
-      ) : (
-        <img
-          onClick={() => setDarkmode(!darkmode)}
-          className='cursor-pointer'
-          src={SunOFF}
-          alt='theme swtich icon'
-        />
-      )}
+    <div className='top-4 md:top-16 left-4 md:left-16 fixed'>
+      <div className='size-16 sm:size-full'>
+        {darkmode ? (
+          <img
+            onClick={() => setDarkmode(!darkmode)}
+            className='cursor-pointer'
+            src={SunON}
+            alt='theme swtich icon'
+          />
+        ) : (
+          <img
+            onClick={() => setDarkmode(!darkmode)}
+            className='cursor-pointer'
+            src={SunOFF}
+            alt='theme swtich icon'
+          />
+        )}
+      </div>
     </div>
   );
 };
