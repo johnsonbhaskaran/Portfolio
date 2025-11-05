7198f31b4d25024ab12b96f6033b3e8a823b9b8e
Johnson Bhaskaran <johnsonb@live.in>
2025-11-05 19:27:07 +0530

    Done: dark mode toggle state logic handle with Context API

---
+++ src/themeContext.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
+
+export const ThemeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode((prev) => !prev);
+  };
+
+  const theme = isDarkMode ? "dark" : "light";
+
+  return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
+};
