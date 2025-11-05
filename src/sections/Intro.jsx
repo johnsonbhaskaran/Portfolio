c9ae41430ee755a4b625347c2536694951da5c12
Johnson Bhaskaran <johnsonb@live.in>
2025-11-05 19:28:12 +0530

    New: Intro section

---
+++ src/sections/Intro.jsx
@@ -0,0 +1,21 @@
+const Intro = () => {
+  return (
+    <div>
+      <div>
+        <p>Welcome to</p>
+        <h1>JohnsonB's portfolio</h1>
+      </div>
+      <div>
+        <img src='' alt='' />
+        <h2>
+          I'm a player <br />
+          🏆 I play <br />
+          Design <br />
+          Frontend <br />
+          Backend <br />
+        </h2>
+      </div>
+    </div>
+  );
+};
+export default Intro;
