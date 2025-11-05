0968d00db3352b7d72ffdb3e1ba6365c297e27bc
Johnson Bhaskaran <johnsonb@live.in>
2025-11-05 19:26:34 +0530

    Done: dark mode toggle fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import "./App.css";
 import Sun from "./components/Sun";
 import Quote from "./sections/Quote";
+import Intro from "./sections/Intro.jsx";
+import Prowess from "./sections/Prowess.jsx";
+import Works from "./sections/Works.jsx";
+import Hire from "./sections/Hire.jsx";
+import Hi from "./sections/Hi.jsx";
+import Footer from "./sections/Footer.jsx";
+import { useTheme } from "./themeContext.jsx";
 
 function App() {
+  const { theme } = useTheme();
+
+  const dark = ` bg-gradient-to-r from-0% from-white via-[#b1b1b1] via-[#cdcdcd] via-36% via-64% to-[#999] to-100% text-[#333]`;
+  const light = ` bg-gradient-to-r from-0% from-[#333] via-[#252525] via-[#1c1c1c] via-23% via-56% to-[#131313] to-100% text-[#ddd]`;
+
   return (
-    <div className='relative'>
+    <div className={`static ${theme === "dark" ? dark : light}`}>
       <Sun />
-      <div className='bg-amber-200 md:mx-auto md:max-w-2/3'>
-        <Quote />
-        <Quote />
+      <div className='md:mx-auto md:max-w-2/3'>
+        {/* <Quote /> */}
+        <Intro />
+        <Prowess />
+        <Works />
+        <Hire />
+        <Hi />
+        <Footer />
       </div>
     </div>
   );
--- src/components/Sun.jsx
+++ src/components/Sun.jsx
@@ -1,27 +1,19 @@
-import { useState } from "react";
 import SunON from "../assets/Sun-ON.svg";
 import SunOFF from "../assets/Sun-OFF.svg";
+import { useTheme } from "../themeContext";
 
 const Sun = () => {
-  const [darkmode, setDarkmode] = useState(false);
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <div className='top-4 md:top-16 left-4 md:left-16 fixed'>
       <div className='size-16 sm:size-full'>
-        {darkmode ? (
-          <img
-            onClick={() => setDarkmode(!darkmode)}
-            className='cursor-pointer'
-            src={SunON}
-            alt='theme swtich icon'
-          />
-        ) : (
-          <img
-            onClick={() => setDarkmode(!darkmode)}
-            className='cursor-pointer'
-            src={SunOFF}
-            alt='theme swtich icon'
-          />
-        )}
+        <img
+          onClick={toggleTheme}
+          className='cursor-pointer'
+          src={theme === "dark" ? SunON : SunOFF}
+          alt='theme swtich icon'
+        />
       </div>
     </div>
   );
