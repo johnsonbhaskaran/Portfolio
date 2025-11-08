beca4972c63189c2a987e300474ac2401ce5610d
Johnson Bhaskaran <johnsonb@live.in>
2025-11-08 22:28:40 +0530

    Minor: tweak in emoji. Inner perfectionist said

--- src/sections/Hire.jsx
+++ src/sections/Hire.jsx
@@ -3,7 +3,7 @@ const Hire = () => {
     <div className='flex flex-col items-center bg-white/0 w-full h-screen'>
       <div className='flex flex-col justify-center w-5xl h-1/4'>
         <p className='text-4xl/16'>Yes you can hire me!</p>
-        <h1 className='text-7xl'>I'm available for Hire.🧑‍💻</h1>
+        <h2 className='text-7xl'>I'm available for Hire.🧑‍💻</h2>
       </div>
       <div className='flex flex-col justify-evenly items-center w-4xl h-3/4 text-3xl'>
         <div className='grid grid-cols-2 w-full cursor-pointer'>
@@ -13,7 +13,7 @@ const Hire = () => {
           <div className='p-6 border border-r-2 border-b-2'>Dribbble</div>
         </div>
         <p>
-          send me an 📫{" "}
+          send me an <span className='text-5xl'>📫 </span>
           <span className={`decoration-wavy underline underline-offset-[12px]`}>
             johnsonb@live.in
           </span>
