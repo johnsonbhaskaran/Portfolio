57a830a1450ef3623b1ba2f2a0446643b54d2df9
Johnson Bhaskaran <johnsonb@live.in>
2025-11-12 18:10:40 +0530

    Done: RWD tablet

--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -7,9 +7,9 @@ const Intro = () => {
 
   return (
     <div className='flex flex-col justify-evenly items-center w-full h-screen'>
-      <div className='flex flex-col justify-evenly w-5xl'>
+      <div className='flex flex-col justify-evenly max-w-xs md:max-w-2xl xl:max-w-5xl'>
         <p className='text-4xl/16'>Welcome to</p>
-        <h1 className='text-7xl'>
+        <h1 className='text-5xl xl:text-7xl'>
           <span
             className={`${
               theme === "light" ? "text-[#FFD700]" : "text-[#000080]"
@@ -20,9 +20,9 @@ const Intro = () => {
           portfolio
         </h1>
       </div>
-      <div className='flex justify-between items-center w-4xl'>
+      <div className='flex xl:flex-row flex-col justify-evenly xl:justify-between items-center w-xs md:w-2xl xl:w-4xl h-3/4'>
         <img src={pic} alt='casual looking Johnson Bhaskaran head shot' />
-        <h2 className='text-7xl/24 text-right'>
+        <h2 className='text-5xl/18 xl:text-7xl/24 text-center xl:text-right'>
           I'm a player <br />
           <img className='inline mr-6 mb-2' src={cup} alt='' />
           I play <br />
