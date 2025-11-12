591beaf5bb76469537dae6dee1880b26d648ce04
Johnson Bhaskaran <johnsonb@live.in>
2025-11-12 19:41:02 +0530

    Done: RWD Mobile devices

--- src/sections/Quote.jsx
+++ src/sections/Quote.jsx
@@ -2,7 +2,7 @@ const Quote = () => {
   return (
     <div className='flex justify-center'>
       <div className='flex flex-col justify-center items-center max-w-xs md:max-w-2xl xl:max-w-5xl h-screen'>
-        <h3 className='mb-6 text-4xl md:text-5xl/18 xl:text-7xl/23 text-center'>
+        <h3 className='mb-6 text-4xl/12 md:text-5xl/18 xl:text-7xl/23 text-center'>
           A pinch of curiosity & a bit of enthusiasm goes a long way
         </h3>
         <p className='w-full md:text-2xl xl:text-4xl text-center xl:text-right'> -unknown</p>
