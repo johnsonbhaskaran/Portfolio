172e6789b4776db305987a468443f988def3ef9e
Johnson Bhaskaran <johnsonb@live.in>
2025-11-12 19:41:13 +0530

    Done: RWD Mobile devices

--- src/sections/Works.jsx
+++ src/sections/Works.jsx
@@ -23,12 +23,12 @@ const Works = () => {
 
   return (
     <div className='h-screen'>
-      <div className='mx-auto max-w-xs md:max-w-2xl xl:max-w-5xl h-1/4 text-5xl xl:text-6xl'>
-        <h2 className='flex items-center h-full'>My Works 💜</h2>
+      <div className='mx-auto max-w-xs md:max-w-2xl xl:max-w-5xl h-1/4 text-[32px] md:text-5xl xl:text-6xl md:text-left text-center'>
+        <h2 className='flex justify-center xl:justify-start items-center h-full'>My Works 💜</h2>
       </div>
-      <div className='mx-auto h-2/3'>
+      <div className='mx-auto h-2/3 md:h-3/5'>
         <div className='flex gap-4 h-full overflow-x-auto no-scrollbar'>
-          <div className='pl-38 xl:pl-106'></div>
+          <div className='pl-8 md:pl-38 xl:pl-106'></div>
           {projectData.map((item) => {
             return (
               <a
@@ -36,13 +36,13 @@ const Works = () => {
                 key={item.id}
                 className={`${
                   theme === "light" ? "bg-[#D9D9D9] text-[#333]" : "bg-[#333] text-[#DDD]"
-                } rounded-[66px] w-5xl h-4/5 xl:h-full shrink-0 relative`}
+                } rounded-[44px] md:rounded-[66px] w-xl md:w-3xl xl:w-5xl h-4/5 xl:h-full shrink-0 relative`}
               >
-                <div className='bottom-0 absolute inset-x-0 p-12'>
-                  <div className='flex justify-between'>
-                    <h3 className='text-4xl xl:text-5xl'>{item.name}</h3>
-                    <div className='flex items-center bg-[salmon] p-2.5 rounded-[6px] cursor-pointer'>
-                      <ArrowUpRight className='size-12' />
+                <div className='bottom-0 absolute inset-x-0 xl:p-12 px-8 md:px-12 py-4 md:py-6'>
+                  <div className='flex justify-between items-center'>
+                    <h3 className='text-2xl md:text-4xl xl:text-5xl'>{item.name}</h3>
+                    <div className='bg-[salmon] mb-4 p-0 md:p-2.5 rounded-[6px] cursor-pointer'>
+                      <ArrowUpRight className='size-10 md:size-12' />
                     </div>
                   </div>
                 </div>
@@ -50,7 +50,7 @@ const Works = () => {
             );
           })}
 
-          <div className='pl-40 xl:pl-106'></div>
+          <div className='pl-8 md:pl-38 xl:pl-106'></div>
         </div>
       </div>
     </div>
