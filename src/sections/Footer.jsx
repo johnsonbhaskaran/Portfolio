5e078c7c4065a60446f2ab77d432aeb46904e596
Johnson Bhaskaran <johnsonb@live.in>
2025-11-12 19:40:21 +0530

    Done: RWD Mobile devices

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
     <div className='flex justify-center items-center pb-16 xl:h-32'>
       <div className='w-xs md:w-2xl xl:w-5xl'>
         <div className='flex xl:flex-row flex-col-reverse justify-between items-center gap-8 xl:gap-0'>
-          <p>copyright © 2025. All rights reserved.</p>
+          <p className='md:text-left text-center'>copyright © 2025. All rights reserved.</p>
           <div className='flex gap-8'>
             <a href='https://www.linkedin.com/in/johnsonbhaskaran/'>
               <img src={`${theme === "light" ? LinkedInDark : LinkedInLight}`} alt='' />
